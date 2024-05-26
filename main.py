8dfd029e00a51b1e008967562119cdb1c8831bf6
lim-hyo-jeong <lim.gadi@gmail.com>
2024-05-26 23:44:55 +0900

    Update model names

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ with col2:
     st.text("")
 
     model_name = st.selectbox("**Select the model**", 
-                              ("gpt-3.5-turbo", "gpt-3.5-turbo-1106", "gpt-4-turbo-preview", "gpt-4-turbo", "gpt-4-1106-preview", "gpt-4", "gpt-4-32k"), 
+                              ("gpt-4o", "gpt-4-turbo", "gpt-4", "gpt-3.5-turbo"), 
                               index=0, 
                               placeholder="Select the model...")
 
